4c1c6e8fc7844d98cc1c7d25fd526b509f7c3cee
Rupesh Gudmalwar <rupesh@quoality.com>
2023-05-02 11:09:45 +0530

    https protocol used

--- src/APIs/index.js
+++ src/APIs/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const geoCodingApi = (searchKey) => {
   return axios.get(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${searchKey}&limit=5&appid=${process.env.REACT_APP_GEO_KEY}`
+    `https://api.openweathermap.org/geo/1.0/direct?q=${searchKey}&limit=5&appid=${process.env.REACT_APP_GEO_KEY}`
   );
 };
 
@@ -14,6 +14,6 @@ export const geoCodingByZipApi = (zipCode, countryCode = "IN") => {
 
 export const fetchWeatherApi = (lat, lon, unit) => {
   return axios.get(
-    `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_GEO_KEY}&units=${unit}`
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_GEO_KEY}&units=${unit}`
   );
 };
